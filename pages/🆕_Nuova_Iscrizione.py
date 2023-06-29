03df94fd3805d31815b7328b7c1ebae5a8585360
tstlnz <testalorenzo@outlook.com>
2023-06-29 14:35:32 +0200

    form

--- pages/🆕_Nuova_Iscrizione.py
+++ pages/🆕_Nuova_Iscrizione.py
@@ -41,36 +41,37 @@ sheet_url = st.secrets["private_gsheets_url"]
 # Set up page
 st.title('Nuova Iscrizione')
 
-name = st.text_input('Nome')
-surname = st.text_input('Cognome')
-email = st.text_input('E-Mail')
-level = st.selectbox('Livello', ['Principiante', 'Intermedio', 'Avanzato'])
-birth = st.date_input('Data di nascita', min_value=datetime.date(1950,1,1))
-phone = st.text_input('Numero di telefono')
+with st.form("my_form"):
+    name = st.text_input('Nome')
+    surname = st.text_input('Cognome')
+    email = st.text_input('E-Mail')
+    level = st.selectbox('Livello', ['Principiante', 'Intermedio', 'Avanzato'])
+    birth = st.date_input('Data di nascita', min_value=datetime.date(1950,1,1))
+    phone = st.text_input('Numero di telefono')
 
-query = f'SELECT * FROM "{sheet_url}"'
-rows = cursor.execute(query)
-emails = [x[4] for x in rows]
-n_subscriptions = len(emails)
+    submitted = st.form_submit_button("Invia iscrizione!")
+    if submitted:
+        query = f'SELECT * FROM "{sheet_url}"'
+        rows = cursor.execute(query)
+        emails = [x[4] for x in rows]
+        n_subscriptions = len(emails)
+        if email in emails:
+            st.error('E-mail già presente nel database')
+        else:
+            ID = create_ID(n_subscriptions)
+            next_match = 'TBD'
+            query = f'INSERT INTO "{sheet_url}" (ID, Name, Surname, Birth, Email, Level, Phone, NextMatch) VALUES ("{ID}", "{name}", "{surname}", "{birth}", "{email}", "{level}", "{phone}", "{next_match}")'
+            cursor.execute(query)
+            connection.commit()
 
-if email in emails:
-    st.error('E-mail già presente nel database')
-else:
-    if st.button('Invia iscrizione!'):
-        ID = create_ID(n_subscriptions)
-        next_match = 'TBD'
-        query = f'INSERT INTO "{sheet_url}" (ID, Name, Surname, Birth, Email, Level, Phone, NextMatch) VALUES ("{ID}", "{name}", "{surname}", "{birth}", "{email}", "{level}", "{phone}", "{next_match}")'
-        cursor.execute(query)
-        connection.commit()
+            text_to_send = mt.Mail(sender=mt.Address(email="no-reply@tdrtennis.it", name="no-reply"),
+                                to=[mt.Address(email= email)],
+                                subject="Iscrizione TDR Tennis",
+                                text="Ciao " + name + ",\n\nGrazie per esserti iscritto a TDR Tennis!\n\n Questo è il tuo codice identificativo per accedere alla tua area personale: " + ID + "\n\nA presto,\n\nTDR Tennis",
+                                category="Welcome")
 
-        text_to_send = mt.Mail(sender=mt.Address(email="no-reply@tdrtennis.it", name="no-reply"),
-                               to=[mt.Address(email= email)],
-                               subject="Iscrizione TDR Tennis",
-                               text="Ciao " + name + ",\n\nGrazie per esserti iscritto a TDR Tennis!\n\n Questo è il tuo codice identificativo per accedere alla tua area personale: " + ID + "\n\nA presto,\n\nTDR Tennis",
-                               category="Welcome")
+            client = mt.MailtrapClient(token= st.secrets["private_token"])
+            client.send(text_to_send)
 
-        client = mt.MailtrapClient(token= st.secrets["private_token"])
-        client.send(text_to_send)
-
-        st.success("Iscrizione effettuata con successo. Riceverai una mail di conferma con il tuo codice identificativo all'indirizzo che hai specificato!")
-        st.balloons()
+            st.success("Iscrizione effettuata con successo. Riceverai una mail di conferma con il tuo codice identificativo all'indirizzo che hai specificato!")
+            st.balloons()

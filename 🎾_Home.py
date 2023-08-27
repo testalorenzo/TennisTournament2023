89273d767dec3e7e843da24a5af08534c999c3cb
tstlnz <testalorenzo@outlook.com>
2023-08-27 07:51:37 -0400

    removed subscriptions

--- 🎾_Home.py
+++ 🎾_Home.py
@@ -15,6 +15,24 @@ image = Image.open('./pages/20230711_094921_0000.png')
 video_file = open('video_home.mp4', 'rb')
 video_bytes = video_file.read()
 
+main_text_old = """
+            Benvenuti nel sito del torneo di tennis 2023 AVIS AIDO!
+
+            A Settembre 2023, presso il Centro Sportivo di Torre de' Roveri - FranVi Bar, verrà organizzato, insieme alle associazioni AVIS e AIDO locali, il secondo Torneo di Tennis AVIS-AIDO.
+            
+            Il torneo si svolgerà dal 01 Settembre al 17 Settembre, con tabellone e format che verranno comunicati successivamente, in relazione al numero di iscritti.
+
+            Le partite si svolgeranno dal Lunedì al Venerdì a partire dalle ore 19, e il Sabato e la Domenica durante l’intera giornata.
+
+            Vi chiediamo gentilmente di iscrivervi a [questo link](https://tdrtennis.streamlit.app/Nuova_Iscrizione), selezionando i giorni in cui sarete disponibili per svolgere le partite.
+
+            Una volta iscritti, riceverete presso la e-mail indicata il vostro ID personale, che vi servirà per rimanere aggiornati su tabellone e prossimi match! 🎾 
+
+            Per essere ufficialmente iscritti vi servirà, oltre alla conferma della registrazione che riceverete via e-mail, anche la conferma di avvenuto pagamento (maggiori informazioni nell’Area Riservata, che trovate a [questo link](https://tdrtennis.streamlit.app/Log_In))
+            
+            Per qualsiasi informazione ci potete contattare alla e-mail: hello@tdrtennis.it
+            """
+
 main_text = """
             Benvenuti nel sito del torneo di tennis 2023 AVIS AIDO!
 
